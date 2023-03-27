5e9924e54a105fb25c9d502abf054b750c77dc91
Ivan <87952318+helpmevania@users.noreply.github.com>
2023-03-27 13:01:45 +0300

    Create chatgpt.py

---
+++ chatgpt.py
@@ -0,0 +1,48 @@
+#not my design
+from .. import loader, utils
+import openai
+
+@loader.tds
+class PromptMod(loader.Module):
+    """Prompts the ChatGPT API's GPT-3.5 model with user's arguments"""
+    strings = {"name": "ChatGPT"}
+    async def client_ready(self, client, db):
+        self.db = db
+
+    async def promptcmd(self, message):
+        """Prompts the ChatGPT API's GPT-3.5 model with user's arguments"""
+        args = utils.get_args_raw(message)
+
+        if not args:
+            await utils.answer(message, "Please provide some text to prompt the ChatGPT API's GPT-3.5 model with.")
+            return
+
+        api_key = self.db.get(__name__, "openai_api_key", None)
+
+        if not api_key:
+            await utils.answer(message, "No OpenAI API key found. Please set one with .setaikey <key>.")
+            return
+
+        await utils.answer(message, "Processing...")
+
+        openai.api_key = api_key
+
+        completion = await utils.run_sync(openai.ChatCompletion.create,
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": args}
+            ]
+        )
+
+        await utils.answer(message, completion.choices[0].message.content)
+
+    async def setaikeycmd(self, message):
+        """Sets the OpenAI API key"""
+        key = utils.get_args_raw(message)
+
+        if not key:
+            await utils.answer(message, "Please provide an OpenAI API key.")
+            return
+
+        self.db.set(__name__, "openai_api_key", key)
+        await utils.answer(message, "OpenAI API key set.")
